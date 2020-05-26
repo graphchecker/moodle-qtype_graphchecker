f956276a36c4d35334922e76e218347773449b1f
Arthur <a.i.v.goethem@tue.nl>
2020-05-26 15:10:51 +0200

    Removed old fsm checks. Simplified basic check to now contain state count so we can use it in the exposition of the tool.

--- checks/fsm/basic.py
+++ checks/fsm/basic.py
@@ -1,29 +1,12 @@
 # Tests for FSMs using igraph.
 
-import igraph
-
-# helper methods
-def _to_igraph(graph):
-    g = igraph.Graph(directed=True)
-    for vertex in graph['vertices']:
-        g.add_vertex(name=vertex['label'])
-    for edge in graph['edges']:
-        g.add_edge(edge['from'], edge['to'], label=edge['label'])
-    return g
-
-def _make_integer_checker(method_name, readable_name):
-    def result(student_answer, sample_answer, preload_answer,
-        expected):
-        g = _to_igraph(student_answer)
-        actual = getattr(g, method_name)()
-        if actual == int(expected):
-            return {'correct': True}
-        else:
-            return {'correct': False,
-                    'feedback': '{0} was {1}, expected {2}'.format(
-                        readable_name, actual, expected)}
-    return result
-
-state_count = _make_integer_checker('vcount', 'State count')
-transition_count = _make_integer_checker('ecount', 'Transition count')
+def state_count(student_answer, sample_answer, preload_answer, expected):
+	states, transitions, initial_states, final_states = student_answer
+	actual = len(states)
+	if actual == int(expected):
+		return {'correct': True}
+	else:
+		return {'correct': False,
+				'feedback': '{0} was {1}, expected {2}'.format(
+					"State count", actual, expected)}
 
