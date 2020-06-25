ead10270ef146f7c761ceb81108d95c5fdc6fda9
joris <joris.d.goddijn@gmail.com>
2020-06-25 13:29:20 +0100

    Added documentation to preprocess

--- checks/petri/preprocess.py
+++ checks/petri/preprocess.py
@@ -2,6 +2,11 @@ from pm4py.objects.petri.petrinet import PetriNet
 
 
 def has_keys(graph):
+    """
+    Checks if graph, a json object, contains all keys needed to be turned in to a petri net.
+    Raises an exception if there is an issue with the json object which makes it unusable.
+    :return: True if there are no issues.
+    """
     # Check general graph
     if 'vertices' not in graph:
         raise Exception("'vertices' not found in graph")
@@ -20,6 +25,7 @@ def has_keys(graph):
             if 'tokens' not in v:
                 raise Exception("There is a place without a number of tokens")
 
+    # Check edges
     for e in graph['edges']:
         if 'from' not in e:
             raise Exception("There is an edge without a 'from'")
@@ -34,10 +40,17 @@ def has_keys(graph):
         if 'perpendicularPart' not in e['bend']:
             raise Exception("There is an edge without a 'bend.perpendicularPart'")
 
+    # No issues
     return True
 
 
 def unique_labels(graph):
+    """
+    Checks if graph, a json object that needs to have the correct keys as check in has_keys,
+    contains any duplicate labels on its vertices.
+    If there are any duplicate labels, an exception is thrown.
+    :return: True if there are no issues.
+    """
     seen_labels = []
     for v in graph['vertices']:
         if v['label'] in seen_labels:
@@ -48,6 +61,12 @@ def unique_labels(graph):
 
 
 def no_empty_label(graph):
+    """
+    Checks if graph, a json object that needs to have the correct keys as check in has_keys,
+    contains a vertex with an empty label.
+    If there is a vertex with an empty label, an exception is thrown.
+    :return:  True if there are no issues.
+    """
     for v in graph['vertices']:
         if v['label'] == '':
             raise Exception("A node has an empty label")
@@ -56,6 +75,12 @@ def no_empty_label(graph):
 
 
 def preprocess(graph):
+    """
+    Takes in graph, a json object representing a petrinet.
+    Checks if graph conforms to the standard, if it doesn't an exception is thrown.
+    If it does, creates a pm4py PetriNet object out of the petri net encoded in the graph object.
+    :return: A PetriNet object.
+    """
     # Check if the given json has all the keys we need
     if not has_keys(graph):
         return None
@@ -68,14 +93,14 @@ def preprocess(graph):
     if not no_empty_label(graph):
         return None
 
-    vertices = graph['vertices']
-    edges = graph['edges']
-
+    # Create PetriNet object
     net = PetriNet("converted_graph")
 
+    # Store an ordered list of the vertices to make it easier to connect them through arcs
     ordered_vertices = []
 
-    for vertex in vertices:
+    # Add all vertices (places and transitions) to net
+    for vertex in graph['vertices']:
         label = vertex['label']
         if vertex['petri_type'] == "place":
             new_place = PetriNet.Place(label)
@@ -91,14 +116,18 @@ def preprocess(graph):
             net.transitions.add(new_transition)
             ordered_vertices.append(new_transition)
 
-    for edge in edges:
+    # Add all edges (arcs) to net
+    for edge in graph['edges']:
         source = ordered_vertices[edge['from']]
         target = ordered_vertices[edge['to']]
 
+        # Create arc object of weight 1
         arc = PetriNet.Arc(source, target, 1)
+        # Add extra properties
         arc.properties['name'] = edge['label']
         arc.properties['bend'] = edge['bend']
 
+        # Add the arc to the net and the source and target objects
         net.arcs.add(arc)
         source.out_arcs.add(arc)
         target.in_arcs.add(arc)
