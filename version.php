118afa6dd01808b3203a2b2ffa38747b4956c8a6
Willem Sonke <willem@wimiso.nl>
2020-06-26 10:06:11 +0000

    Bump plugin version

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2020061700;
+$plugin->version  = 2020062600;
 $plugin->requires = 2015051200;
 $plugin->cron = 0;
 $plugin->component = 'qtype_graphchecker';
