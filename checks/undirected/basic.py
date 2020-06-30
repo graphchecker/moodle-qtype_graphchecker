d2001dd6c137775914c0c7a0a02a05b73e61a3f3
Arthur <a.i.v.goethem@tue.nl>
2020-06-30 12:30:10 +0200

    UPDATE: Moved BST checks to one file. UPDATE: Cleaned up BST checks and split into BT layout and BST layout. UPDATE: Allowed all binary tree checks to work with trees drawn both upwards and downwards. ADD:    Added checks for: sum edge weights.

--- checks/undirected/basic.py
+++ checks/undirected/basic.py
@@ -19,16 +19,36 @@ edge_count = _make_integer_checker('ecount', 'Edge count')
 vertex_count = _make_integer_checker('vcount', 'Vertex count')
 
 def connected(student_answer, sample_answer, preload_answer):
-	if (student_answer.is_connected(False)):
-		return {'correct': True}
-	else:
-		return {'correct': False,
-				'feedback': ('Graph is not connected')}
+    if (student_answer.is_connected(False)):
+        return {'correct': True}
+    else:
+        return {'correct': False,
+                'feedback': ('Graph is not connected')}
 
-def exact_same(student_answer, sample_answer, preload_answer, expected):
-	if (student_answer.isomorphic(expected)):
-		return {'correct': True}
-	else:
-		return {'correct': False,
-				'feedback': ('Incorrect answer given')}
-	
+def sumEdgeWeights(student_answer, sample_answer, preload_answer, expected):
+    weight = 0
+    for e in student_answer.es:
+        edgeweight = int(e['label'])
+        weight = weight + edgeweight
+    if weight == expected:
+        return {'correct': True}
+    else:
+        return {'correct': False, 'feedback': 'The sum of edge weights did not match the required sum of edge weights.'}
+
+def layout(student_answer, sample_answer, preload_answer):
+    if not student_answer.isomorphic(sample_answer):
+        return {'correct': False,
+                'feedback': 'Your graph was not isomorphic to the given answer'}
+
+    #check x-order and y-order layout is the same
+    xListStudent = sorted(student_answer.vs, key=lambda v: v["x"], reverse=False)
+    yListStudent = sorted(student_answer.vs, key=lambda v: v["y"], reverse=False)
+    
+    xListSample = sorted(sample_answer.vs, key=lambda v: v["x"], reverse=False)
+    yListSample = sorted(sample_answer.vs, key=lambda v: v["y"], reverse=False)
+    
+    for ((stud1, stud2), (samp1, samp2)) in zip((zip(xListStudent,xListStudent[1:])), zip(yListStudent,yListStudent[1:])):
+        if not (stud1["x"] < stud2["x"]) == (samp1["x"] < samp2["x"]):
+            return {'correct': False,
+                'feedback': 'The general layout of your graph did not match the sample answer.'}
+    return {'correct': True}
--- checks/undirected/rooted_tree.py
+++ checks/undirected/rooted_tree.py
@@ -7,40 +7,30 @@ def filter_orig_name(v):
     return v['name'].split("_")[1]
 
 #helper
-def splitParentChildren(v):
+def splitParentChildren(v, down):
     parents = []
     children = []
     
     for w in v.neighbors():
-        if w['y'] < v['y']:
+        if (w['y'] < v['y'] and down) or (w['y'] > v['y'] and not down):
             parents.append(w)
-        elif w['y'] > v['y']:
+        elif (w['y'] > v['y'] and down) or (w['y'] < v['y'] and not down):
             children.append(w)
         else:
             return None
     return (parents, children)
-    
-#helper - return root or None if multiple roots or unclear layout
-def findRoot(student_answer):
-    root = None
-    for v in student_answer.vs:
-        split = splitParentChildren(v)
-        if split == None:
-            return None
-        (par, chil) = split
-        if len(par) == 0 and root == None:
-            print(filter_orig_name(v))
-            root = v
-        elif len(par) == 0 and not root == None:
-            print("double")
-            print(filter_orig_name(v))
-            return None
-    return root
 
-def BinaryTreeStructure(student_answer, sample_answer, preload_answer):
+def BinaryTree(student_answer, sample_answer, preload_answer, downwards):
+    if downwards == "top":
+        down = True
+    elif downwards == "bottom":
+        down = False
+    else:
+        raise Exception('Unknown parameter value supplied. Contact support.')
+
     root = 0
     for v in student_answer.vs:
-        split = splitParentChildren(v)
+        split = splitParentChildren(v, down)
         if split == None:
             return {'correct': False,
                     'feedback': ('Not a binary tree: two adjacent nodes are at the same height and have no clear relationship.')}
@@ -58,13 +48,62 @@ def BinaryTreeStructure(student_answer, sample_answer, preload_answer):
                 'feedback': ('Not a binary tree: there is more than one root in the drawing.')}
     return {'correct': True }
 
-def NodeDepth(student_answer, sample_answer, preload_answer, label, depth):
+def BinarySearchTree(student_answer, sample_answer, preload_answer, downwards):
+    if downwards == "top":
+        down = True
+    elif downwards == "bottom":
+        down = False
+    else:
+        raise Exception('Unknown parameter value supplied. Contact support.')
+    
+    root = 0
+    for v in student_answer.vs:
+        split = splitParentChildren(v, down)
+        if split == None:
+            return {'correct': False,
+                    'feedback': ('Not a binary tree: two adjacent nodes are at the same height and have no clear relationship.')}
+        (par, chil) = split
+        if len(par) > 1:
+            return {'correct': False,
+                    'feedback': ('Not a binary tree: node ' + filter_orig_name(v) + ' has two parents.')}
+        elif len(par) == 0:
+            root += 1
+        if len(chil) > 2:
+            return {'correct': False,
+                    'feedback': ('Not a binary tree: node ' + filter_orig_name(v) + ' has more than two children.')}
+        valueV = int(filter_orig_name(v))
+        for w in chil:
+            valueW = int(filter_orig_name(w))
+            vIsLeft    = v['x'] < w['x']
+            equal      = valueV == valueW
+            vIsSmaller = valueV < valueW
+            if (not equal and vIsLeft != vIsSmaller):
+                direction = 'left'
+                size      = ' > '
+                if (vIsLeft):
+                    direction = 'right'
+                    size      = ' < '
+                return {'correct': False,
+                        'feedback': ("Layout mistake: Vertex " + filter_orig_name(w) + ' is a ' + direction + ' child of vertex ' + filter_orig_name(v) + ', but ' + filter_orig_name(w) + size + filter_orig_name(v) + ".")}
+    if root > 1:
+        return {'correct': False,
+                'feedback': ('Not a binary tree: there is more than one root in the drawing.')}
+    return {'correct': True }
+
+def NodeDepth(student_answer, sample_answer, preload_answer, label, depth, downwards):
+    if downwards == "top":
+        down = True
+    elif downwards == "bottom":
+        down = False
+    else:
+        raise Exception('Unknown parameter value supplied. Contact support.')
+        
     for v in student_answer.vs:
         if filter_orig_name(v) == label:
             vertex = v
             dep = 0
             while not v == None:
-                split = splitParentChildren(v)
+                split = splitParentChildren(v, down)
                 if split == None:
                     return {'correct': False,
                             'feedback': ('There is a problem with the layout that makes distinguishing the tree impossible.')}
