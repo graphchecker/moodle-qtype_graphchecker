ded698c53fd7c4bb8a6999344a2913ffa3ffcb07
Arthur <a.i.v.goethem@tue.nl>
2020-07-02 13:49:01 +0200

    temp commit to merge master in.

--- checks/petri/basic.py
+++ checks/petri/basic.py
@@ -2,7 +2,7 @@ import networkx as nx
 from pm4py.objects.petri.networkx_graph import create_networkx_directed_graph, create_networkx_undirected_graph
 
 
-def connected(student_answer, sample_answer, preload_answer):
+def connected(student_answer):
     # get networkx graph
     normal_graph, _, _, table = create_networkx_undirected_graph(student_answer, None, None)
     if nx.is_connected(normal_graph):
@@ -12,7 +12,7 @@ def connected(student_answer, sample_answer, preload_answer):
                 'feedback': 'Graph is not connected.'}
 
 
-def strongly_connected(student_answer, sample_answer, preload_answer):
+def strongly_connected(student_answer):
     # Use networkx to get scc
     normal_graph, table = create_networkx_directed_graph(student_answer)
     components = nx.algorithms.components.strongly_connected_components(normal_graph)
@@ -34,7 +34,7 @@ def strongly_connected(student_answer, sample_answer, preload_answer):
                             'It has {0} strongly connected components.'.format(num_components)}
 
 
-def transition_degree_one(student_answer, sample_answer, preload_answer):
+def transition_degree_one(student_answer):
     for transition in student_answer.transitions:
         in_degree = len(transition.in_arcs)
         out_degree = len(transition.out_arcs)
@@ -46,13 +46,13 @@ def transition_degree_one(student_answer, sample_answer, preload_answer):
     return {'correct': True}
 
 
-def node_on_shortest_path(student_answer, sample_answer, preload_answer, label_a, label_b, label_c):
+def node_on_shortest_path(student_answer, label_a, label_b, label_c):
     # TODO try their implementation and see what it does
     return {'correct': False,
             'feedback': 'Not implemented yet.'}
 
 
-def node_label_exists(student_answer, sample_answer, preload_answer, label):
+def node_label_exists(student_answer, label):
     for place in student_answer.places:
         if place.name == label:
             return {'correct': True}
@@ -64,7 +64,7 @@ def node_label_exists(student_answer, sample_answer, preload_answer, label):
             'feedback': 'No place or transition found with label {0}'.format(label)}
 
 
-def node_empty_label(student_answer, sample_answer, preload_answer):
+def node_empty_label(student_answer):
     for place in student_answer.places:
         if place.name == "":
             return {'correct': True}
@@ -76,7 +76,7 @@ def node_empty_label(student_answer, sample_answer, preload_answer):
             'feedback': 'Net has no node with an empty label.'}
 
 
-def arc_missing(student_answer, sample_answer, preload_answer, label_a, label_b):
+def arc_missing(student_answer, label_a, label_b):
     for arc in student_answer.arcs:
         node_a = arc.source.name
         node_b = arc.target.name
@@ -89,7 +89,7 @@ def arc_missing(student_answer, sample_answer, preload_answer, label_a, label_b)
     return {'correct': True}
 
 
-def arc_has_label(student_answer, sample_answer, preload_answer, label_a, label_b, label_arc):
+def arc_has_label(student_answer, label_a, label_b, label_arc):
     for arc in student_answer.arcs:
         if arc.properties['name'] != label_arc:
             continue
@@ -104,7 +104,7 @@ def arc_has_label(student_answer, sample_answer, preload_answer, label_a, label_
                         ' with arc label {2}.'.format(label_a, label_b, label_arc)}
 
 
-def at_most_one_arc(student_answer, sample_answer, preload_answer, label_a, label_b):
+def at_most_one_arc(student_answer, label_a, label_b):
     count = 0
     for arc in student_answer.arcs:
         if arc.source.name == label_a and arc.target.name == label_b:
