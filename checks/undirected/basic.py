41fa74d05bdd6b7f5cf5d8d600cd7cc9ffe845cc
Arthur <a.i.v.goethem@tue.nl>
2020-08-11 14:34:45 +0200

    UPDATED: edge and vertex count to also work for only highlighted vertices/edges. ADDED:   check for undirected that checks if a cycle is present (in all or only the highlighted edges)

--- checks/undirected/basic.py
+++ checks/undirected/basic.py
@@ -18,8 +18,55 @@ def _make_integer_checker(method_name, readable_name):
                         readable_name, actual, expected)}
     return result
 
-edge_count = _make_integer_checker('ecount', 'Edge count')
-vertex_count = _make_integer_checker('vcount', 'Vertex count')
+def edge_count(student_answer, expected, highlighted):
+    if highlighted == "only highlighted edges":
+        onlyHighlighted = True
+    else:
+        onlyHighlighted = False
+    count = 0
+    for e in student_answer.es:
+        if (not onlyHighlighted or e['highlighted']):
+            count = count + 1
+    if count == expected:
+        return {'correct': True}
+    else:
+        return {'correct': False,
+                'feedback': 'Number of edges does not match expected number'}
+
+def vertex_count(student_answer, expected, highlighted):
+    if highlighted == "only highlighted vertices":
+        onlyHighlighted = True
+    else:
+        onlyHighlighted = False
+    count = 0
+    for v in student_answer.vs:
+        if (not onlyHighlighted or v['highlighted']):
+            count = count + 1
+    if count == expected:
+        return {'correct': True}
+    else:
+        return {'correct': False,
+                'feedback': 'Number of vertices does not match expected number'}
+
+def no_cycles(student_answer, highlighted):
+    if highlighted == "only highlighted vertices":
+        onlyHighlighted = True
+    else:
+        onlyHighlighted = False
+
+    tree_copy = student_answer.copy()
+    if onlyHighlighted:
+        tree_copy.es.select(highlighted=False).delete()
+        span_tree = tree_copy.spanning_tree()
+    else:
+        span_tree = student_answer.spanning_tree()
+    edgespan  = getattr(span_tree, "ecount")()
+    edgegraph = getattr(tree_copy, "ecount")()
+    if edgespan == edgegraph:
+        return {'correct': True}
+    else:
+        return {'correct': False,
+                'feedback': 'Your answer contains a cycle'}
 
 def connected(student_answer):
     if (student_answer.is_connected(False)):
