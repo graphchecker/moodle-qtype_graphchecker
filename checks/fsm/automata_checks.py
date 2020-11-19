01a2e922f045a5e41d059431ed4bb9e12cbaaac8
Willem Sonke <willem@wimiso.nl>
2020-11-19 15:29:11 +0000

    Reimplement FSM state count check
    Also properly fail a check if an exception is raised in the automata
    library.

--- checks/fsm/automata_checks.py
+++ checks/fsm/automata_checks.py
@@ -4,7 +4,11 @@ from automata.notebook import parse_dfa, generate_language, parse_word_list, com
 
 def check_dfa_language_from_words(student_answer, word_list, length):
     word_list = " ".join(word_list)
-    A = parse_dfa(student_answer)
+    try:
+        A = parse_dfa(student_answer)
+    except RuntimeError as e:
+        return {'correct': False,
+                'feedback': str(e)}
     A_words = generate_language(A, length)
     words = parse_word_list(word_list)
     feedback = compare_languages(A_words, words)
@@ -14,8 +18,12 @@ def check_dfa_language_from_words(student_answer, word_list, length):
         return {'correct': False,
                 'feedback': " / ".join(feedback)}
 
-def check_dfa_language_from_words(student_answer, other, length):
-    A = parse_dfa(student_answer)
+def check_dfa_language_from_answer(student_answer, other, length):
+    try:
+        A = parse_dfa(student_answer)
+    except RuntimeError as e:
+        return {'correct': False,
+                'feedback': str(e)}
     A_words = generate_language(A, length)
     B = parse_dfa(other)
     B_words = generate_language(B, length)
@@ -25,3 +33,18 @@ def check_dfa_language_from_words(student_answer, other, length):
     else:
         return {'correct': False,
                 'feedback': " / ".join(feedback)}
+
+def check_dfa_max_states(student_answer, max_states):
+    try:
+        A = parse_dfa(student_answer)
+    except RuntimeError as e:
+        return {'correct': False,
+                'feedback': str(e)}
+    num_states = len(A.Q)
+    if num_states <= max_states:
+        return {'correct': True}
+    else:
+        return {'correct': False,
+                'feedback': "You used too many states (at most " +
+                        str(max_states) + " allowed)"}
+
