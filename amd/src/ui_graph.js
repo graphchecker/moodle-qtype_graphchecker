83c5c8f9d8ce05d6948d4b957b03f86888187d49
Raoef Taki <r.taki@student.tue.nl>
2021-02-15 11:51:26 +0100

    Test with graph canvas code in separate file
    Loading the graph_canvas.js code now works in ui_graph.js
    
    (Temporarily) reverted types.json

--- amd/src/ui_graph.js
+++ amd/src/ui_graph.js
@@ -47,74 +47,10 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-
 define(['jquery', 'qtype_graphchecker/graphutil', 'qtype_graphchecker/graphelements',
-    'qtype_graphchecker/ui_toolbar', 'qtype_graphchecker/toolbar_elements'],
-    function($, util, elements, ui_toolbar, toolbar_elements) {
-
-    /***********************************************************************
-     *
-     * A GraphCanvas is a wrapper for a Graph's HTML canvas
-     * object.
-     *
-     ************************************************************************/
-
-    function GraphCanvas(parent, canvasId, w, h) {
-        // Constructor, given the Graph that owns this canvas, the
-        // required canvasId and the height and width of the wrapper that
-        // encloses the Canvas.
-
-        this.parent = parent;
-        this.canvas = $(document.createElement("canvas"));
-        this.canvas.attr({
-            id:         canvasId,
-            class:      "graphchecker_graphcanvas",
-            tabindex:   1 // So canvas can get focus.
-        });
-        this.canvas.css({'background-color': util.Color.WHITE});
-
-        this.canvas.on('mousedown', function(e) {
-            return parent.mousedown(e);
-        });
-
-        this.canvas.on('mouseup', function(e) {
-            return parent.mouseup(e);
-        });
-
-        // Added so that the mouseup event is executed when the mouse leaves the graph UI canvas
-        this.canvas.on('mouseenter', function(e) {
-            return parent.mouseenter(e);
-        });
-
-        // Added so that the mouseup event is executed when the mouse leaves the graph UI canvas
-        this.canvas.on('mouseleave', function(e) {
-            return parent.mouseleave(e);
-        });
-
-        this.canvas.on('keydown', function(e) {
-            return parent.keydown(e);
-        });
-
-        this.canvas.on('keyup', function(e) {
-            return parent.keyup(e);
-        });
-
-        this.canvas.on('mousemove', function(e) {
-            return parent.mousemove(e);
-        });
-
-        this.canvas.on('keypress', function(e) {
-            return parent.keypress(e);
-        });
-
-        this.resize = function(w, h) {
-            // Resize to given dimensions.
-            this.canvas.attr("width", w);
-            this.canvas.attr("height", h);
-        };
-
-        this.resize(w, h);
-    }
+        'qtype_graphchecker/graph_components/graph_canvas', 'qtype_graphchecker/ui_toolbar',
+        'qtype_graphchecker/toolbar_elements'],
+    function($, util, elements, graph_canvas, ui_toolbar, toolbar_elements) {
 
     /***********************************************************************
      *
@@ -201,7 +137,7 @@ define(['jquery', 'qtype_graphchecker/graphutil', 'qtype_graphchecker/grapheleme
         this.readOnly = this.textArea.prop('readonly');
         this.templateParams = templateParams;
         this.uiWrapper = uiWrapper;
-        this.graphCanvas = new GraphCanvas(this, this.canvasId, width, height);
+        this.graphCanvas = new graph_canvas.GraphCanvas(this, this.canvasId, width, height);
 
         this.helpOverlayId = 'graphcanvas_overlay_' + textareaId;
         this.helpOverlay = new HelpOverlay(this, this.helpOverlayId, this.uiWrapper);
