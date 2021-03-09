bd504248e837ae6f33ba6e45ca589aa61e8127f0
Willem Sonke <willem@wimiso.nl>
2021-03-09 10:04:05 +0100

    Let the tester run all tests in a directory
    Fixes #170.

--- tester/run_unit_tests.py
+++ tester/run_unit_tests.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-# Script to run a set of unit tests 
+# Script to run a set of unit tests
 
 import json
 import os
+from pathlib import Path
 import sys
 import traceback
 import urllib.parse
@@ -15,37 +16,43 @@ if len(sys.argv) < 2:
 	sys.exit(1)
 
 def run_test(name, test_spec):
-    if len(test_spec) != 3:
-        print('\033[1m\033[91mfail ' + name + '\033[0m')
-        print('    test file invalid')
-        return
-    [answer, test, expected] = test_spec
-    params = {
-        "graph": answer,
-        "checks": '[' + test + ']'
-    }
-    url = "http://localhost:8080/test?{}".format(urllib.parse.urlencode(params))
-    result = urllib.request.urlopen(url).read()
-    result = json.loads(result)
-    if result['type'] == 'preprocess_fail':
-        print('\033[1m\033[91mfail ' + name + '\033[0m')
-        print('    preprocess fail: ' + result['feedback'])
-        return
-    
-    if result['results'][0]['correct'] and expected == 'fail':
-        print('\033[1m\033[91mfail ' + name + '\033[0m')
-        print('    expected fail, test passed')
-        return
-    
-    if not result['results'][0]['correct'] and expected == 'pass':
-        print('\033[1m\033[91mfail ' + name + '\033[0m')
-        print('    expected pass, test failed')
-        return
-    
-    print('\033[1m\033[92mpass\033[0m ' + name + '\033[0m')
+	if len(test_spec) != 3:
+		print('\033[1m\033[91mfail ' + name + '\033[0m')
+		print('    test file invalid')
+		return
+	[answer, test, expected] = test_spec
+	params = {
+			"graph": answer,
+			"checks": '[' + test + ']'
+			}
+	url = "http://localhost:8080/test?{}".format(urllib.parse.urlencode(params))
+	result = urllib.request.urlopen(url).read()
+	result = json.loads(result)
+	if result['type'] == 'preprocess_fail':
+		print('\033[1m\033[91mfail ' + name + '\033[0m')
+		print('    preprocess fail: ' + result['feedback'])
+		return
+
+	if result['results'][0]['correct'] and expected == 'fail':
+		print('\033[1m\033[91mfail ' + name + '\033[0m')
+		print('    expected fail, test passed')
+		return
+
+	if not result['results'][0]['correct'] and expected == 'pass':
+		print('\033[1m\033[91mfail ' + name + '\033[0m')
+		print('    expected pass, test failed')
+		return
+
+	print('\033[1m\033[92mpass\033[0m ' + name + '\033[0m')
 
 for i in range(1, len(sys.argv)):
-    testfile_name = sys.argv[i]
-    with open(testfile_name) as testfile:
-        lines = [line.strip() for line in testfile if line.strip() != '']
-    run_test(testfile_name, lines)
+	testfile_name = sys.argv[i]
+	try:
+		with open(testfile_name) as testfile:
+			lines = [line.strip() for line in testfile if line.strip() != '']
+		run_test(testfile_name, lines)
+	except IsADirectoryError:
+		for path in Path(testfile_name).rglob('*.txt'):
+			with open(path) as testfile:
+				lines = [line.strip() for line in testfile if line.strip() != '']
+			run_test(str(path), lines)
