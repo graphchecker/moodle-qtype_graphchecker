5feb6bf084637b514aa3e8a2c050bbf9e52ea19d
Willem Sonke <willem@wimiso.nl>
2021-04-22 18:02:15 +0200

    Basic support for PDAs

---
+++ checks/pda/equivalence.py
@@ -0,0 +1,36 @@
+# Tests for PDAs.
+
+from automata.notebook import parse_pda, generate_language, parse_word_list, compare_languages
+
+def language_equivalence_automaton(student_answer, other, length):
+    try:
+        A = parse_pda(student_answer)
+    except RuntimeError as e:
+        return {'correct': False,
+                'feedback': str(e)}
+    A_words = generate_language(A, length)
+    B = parse_pda(other)
+    B_words = generate_language(B, length)
+    feedback = compare_languages(A_words, B_words)
+    if len(feedback) == 0:
+        return {'correct': True}
+    else:
+        return {'correct': False,
+                'feedback': " / ".join(feedback)}
+
+def language_equivalence_words(student_answer, word_list, length):
+    word_list = " ".join(word_list)
+    try:
+        A = parse_pda(student_answer)
+    except RuntimeError as e:
+        return {'correct': False,
+                'feedback': str(e)}
+    A_words = generate_language(A, length)
+    words = parse_word_list(word_list)
+    feedback = compare_languages(A_words, words)
+    if len(feedback) == 0:
+        return {'correct': True}
+    else:
+        return {'correct': False,
+                'feedback': " / ".join(feedback)}
+
---
+++ checks/pda/preprocess.py
@@ -0,0 +1,32 @@
+def preprocess(graph):
+    text = ""
+
+    final_states = []
+    state_labels_seen = set()
+    for state in graph['vertices']:
+        if (not 'label' in state) or (state['label'] == ''):
+            raise Exception("Automaton contains a state without a label")
+        label = state['label']
+        if label in state_labels_seen:
+            raise Exception("Automaton contains duplicate state '" + label + "'")
+        state_labels_seen.add(label)
+        if state['initial']:
+            text += "initial " + state['label'] + "\n"
+        if state['final']:
+            final_states.append(state['label'])
+    if len(final_states) > 0:
+        text += "final " + " ".join(final_states) + "\n"
+
+    for transition in graph['edges']:
+        if transition['from'] == -1:
+            continue  # ignore the initial edge
+        
+        if (not 'label' in transition) or (transition['label'] == ''):
+            raise Exception("Automaton contains a transition without a label")
+        
+        p = graph['vertices'][transition['from']]['label']
+        q = graph['vertices'][transition['to']]['label']
+        a = transition['label']
+        text += p + " " + q + " " + a + "\n"
+
+    return text
