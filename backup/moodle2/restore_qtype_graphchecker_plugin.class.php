8cbc647eb067e03d5bd9e8577bbb2e189a1f975f
Willem Sonke <willem@wimiso.nl>
2021-05-31 22:11:21 +0000

    Fix question backup
    It turns out that to backup the extra_question_fields, one still needs
    to inherit from backup_qtype_extrafields_plugin manually.
    
    We found this out by looking at Moodle's built-in shortanswer question
    type, which does the same thing.
    
    Fixes #29.

---
+++ backup/moodle2/restore_qtype_graphchecker_plugin.class.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of GraphChecker - https://github.com/graphchecker
+//
+// GraphChecker is based on CodeRunner by Richard Lobb et al.
+// See https://coderunner.org.nz/
+//
+// GraphChecker is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GraphChecker is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with GraphChecker.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Provides the information to restore GraphChecker questions.
+ *
+ * Based on backup/moodle2/restore_qtype_shortanswer_plugin.class.php
+ *
+ * @package   qtype_graphchecker
+ * @copyright TU Eindhoven, The Netherlands
+ * @copyright 2010 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class restore_qtype_graphchecker_plugin extends restore_qtype_extrafields_plugin {
+
+    public function process_graphchecker($data) {
+        $this->really_process_extra_question_fields($data);
+    }
+}
